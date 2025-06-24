fd6abe6803eaa5ae09b5601f4908332fe7a930d3
­TeddyKwak <joke79@hanyang.ac.kr>
2025-06-24 23:56:19 +0900

    redis cache #1

--- src/main/java/com/teddy/jwt_authflow/config/RedisConfiguration.java
+++ src/main/java/com/teddy/jwt_authflow/config/RedisConfiguration.java
@@ -1,13 +1,19 @@
 package com.teddy.jwt_authflow.config;
 
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager.RedisCacheManagerBuilder;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext.SerializationPair;
+import org.springframework.data.redis.serializer.RedisSerializer;
 
 @Configuration
 public class RedisConfiguration {
@@ -24,5 +30,26 @@ public class RedisConfiguration {
         redisTemplate.setConnectionFactory(redisConnectionFactory);
         redisTemplate.setDefaultSerializer(new Jackson2JsonRedisSerializer<>(Object.class));
         return redisTemplate;
+    }
+
+    @Bean
+    @Primary
+    public CacheManager listCacheManager(final RedisConnectionFactory redisConnectionFactory) {
+        return build(redisConnectionFactory, new Jackson2JsonRedisSerializer<>(Object.class));
+    }
+
+    @Bean
+    public CacheManager objectCacheManager(final RedisConnectionFactory redisConnectionFactory) {
+        return build(redisConnectionFactory, RedisSerializer.json());
+    }
+
+    private CacheManager build(final RedisConnectionFactory redisConnectionFactory, final RedisSerializer<?> redisSerializer) {
+        final var serializer = SerializationPair.fromSerializer(redisSerializer);
+        return RedisCacheManagerBuilder.fromConnectionFactory(redisConnectionFactory)
+                .cacheDefaults(
+                        RedisCacheConfiguration.defaultCacheConfig()
+                                .disableCachingNullValues()
+                                .serializeValuesWith(serializer))
+                .build();
     }
 }
